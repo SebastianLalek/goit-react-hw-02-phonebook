8c2bb1fbc9f3754d696d38876b4679bcf47ad702
Sebastian Lalek <chemiczq@gmail.com>
2023-11-04 12:43:09 +0100

    add filter styles

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,7 +1,9 @@
+import css from './filter.module.css';
+
 export default function Filter({ onChange, onSubmit }) {
   return (
     <form onSubmit={onSubmit}>
-      <label>
+      <label className={css.label}>
         <p>Find your contact by name</p>
         <input type="text" name="filter" onChange={onChange} />
       </label>
